10b0fc55ec637c62d202ad5b5a423dd643b6764b
vladun13 <nemyrovsky1311vlad@gmail.com>
2018-10-05 00:00:55 +0300

    first commit

--- source/components/Feed/index.js
+++ source/components/Feed/index.js
@@ -5,17 +5,33 @@ import React, { Component } from 'react';
 import StatusBar from 'components/StatusBar';
 import Composer from 'components/Composer';
 import Post from 'components/Post';
+import Spinner from 'components/Spinner';
 
 //Instruments
 import Styles from './styles.m.css';
 
 export default class Feed extends Component {
+	state = {
+		posts: [
+		{ id: '123', comment: 'Hi there!', created: 1526825076849 },
+		{ id: '456', comment: 'Hi dear!', created: 1526825076855 }
+	],
+		isSpinning: {
+			id: '789'
+		}
+}
 	render() {
+		const { posts, isSpinning } = this.state;
+		const postsJSX = posts.map((post) => {
+			return <Post key = { post.id } { ...post } />
+		})
+
 		return (
 			<section className = { Styles.feed }>
+				<Spinner isSpinning />
 				<StatusBar />
 				<Composer />
-				<Post />
+				{postsJSX}
 			</section>
 			)
 	}
--- source/components/Post/index.js
+++ source/components/Post/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import moment from 'moment';
+import PropTypes from 'prop-types';
 
 //Components
 import { Consumer } from 'components/HOC/withProfile';
@@ -8,15 +9,20 @@ import { Consumer } from 'components/HOC/withProfile';
 import Styles from './styles.m.css';
 
 export default class Post extends Component {
+    static PropTypes = {
+        comment: PropTypes.string.isRequired,
+        created: PropTypes.number.isRequired
+    }
 	render () {
+        const { comment, created } = this.props;
 		return (
             <Consumer>
                 {(context) => (
                     <section className = { Styles.post }>
                         <img src= { context.avatar } alt=""/>
                         <a>{`${ context.currentUserFirstName} ${ context.currentUserLastName }`}</a>
-                        <time>{moment().format('MMMM D h:mm:ss')}</time>
-                        <p>Howdy!</p>
+                        <time>{moment.unix(created).format('MMMM D h:mm:ss')}</time>
+                        <p>{comment}</p>
                     </section>
                 )}
             </Consumer>
