9308119cbbbb243aac87a6dc1bd97adfb3b26dc7
vladun13 <nemyrovsky1311vlad@gmail.com>
2018-10-27 23:11:35 +0300

    first commit

--- source/components/Feed/index.js
+++ source/components/Feed/index.js
@@ -15,6 +15,7 @@ import Composer from 'components/Composer';
 import Post from 'components/Post';
 import Spinner from 'components/Spinner';
 import Postman from 'components/Postman';
+import Counter from 'components/Counter';
 
 //Instruments
 import Styles from './styles.m.css';
@@ -206,7 +207,7 @@ export default class Feed extends Component {
 			 		timeout = { {
 			 			enter: 500,
 			 			exit: 400,
-			 		} }
+			 		}}
 			 		>
 					<Catcher>
 						<Post 
@@ -238,6 +239,7 @@ export default class Feed extends Component {
 					onEntered = { this._animatePostmanEntered }>
 					<Postman />
 				</Transition>
+				<Counter count = { postsJSX.length }/>
 				<TransitionGroup>{postsJSX}</TransitionGroup>
 			</section>
 			)
---
+++ source/instruments/index.test.js
@@ -0,0 +1,56 @@
+//Core
+import { sum, delay, getUniqueID, getFullApiUrl } from './';
+
+
+describe('instruments:', () => {
+	test('sum function should be a function', () => {
+		expect(sum).toBeInstanceOf(Function);
+	});
+
+	test('sum function should throw, when called with non-number type as second argument', () => {
+		expect(() => sum(2, 'Hi')).toThrow();
+	});
+
+	test('sum function should throw, when called with non-number type as second argument', () => {
+		expect(() => sum('Hi', 2)).toThrow();
+	});
+	
+	test('sum function should return an addition of two arguments passed', () => {
+		expect(sum(2, 3)).toBe(5);
+		expect(sum(1, 8)).toMatchSnapshot();
+	});
+
+	test('delay function should return a resolved promise', async () => {
+			await expect(delay()).resolves.toBeUndefined();
+	});
+
+	test('getUniqueID function should be a function', () => {
+			expect(getUniqueID).toBeInstanceOf(Function);
+	});
+
+
+	test('getUniqueID function should throw, when called with non-number type as an argument', () => {
+		expect(() => getUniqueID('Hi')).toThrow();
+	});
+
+	test('getUniqueID function should produce a string of a desired given length', () => {
+		expect(typeof getUniqueID()).toBe('string');
+		expect(getUniqueID(5)).toHaveLength(5);
+		expect(getUniqueID(13)).toHaveLength(13);
+	});
+
+	test('getFullApiUrl function should be a function', () => {
+		expect(getFullApiUrl).toBeInstanceOf(Function);
+	});
+
+	test('getFullApiUrl function should throw, when called with non-number type as an argument', () => {
+		expect(() => getFullApiUrl('Hi', 2)).toThrow();
+		expect(() => getFullApiUrl(2, 'Hi')).toThrow();
+		expect(() => getFullApiUrl(2, 2)).toThrow();
+	});
+
+	test('getFullApiUrl function should return FullApiUrl', () => {
+		expect(getFullApiUrl('ApiUrl', 'GROUP_ID')).toBe('ApiUrl/GROUP_ID');
+		expect(getFullApiUrl('ApiUrl', 'GROUP_ID')).toMatchSnapshot();
+	})
+})
